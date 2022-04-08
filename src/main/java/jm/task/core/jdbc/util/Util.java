a9bb1cbb5348b9c0a467af945b97e06fa76a53c3
Maxim Okorokov <okoro99@mail.ru>
2022-04-08 16:00:51 +0300

    Util-подключение к бд

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+   private static final String dbURL = "jdbc:mysql://localhost:3306/mydb1";
+   private static final String dbUserName = "root";
+   private static final String dbPassword = "Maxim_180299";
+
+
+    public static Connection getConnection(){
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(dbURL,dbUserName,dbPassword);
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Не подключился к БД");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
