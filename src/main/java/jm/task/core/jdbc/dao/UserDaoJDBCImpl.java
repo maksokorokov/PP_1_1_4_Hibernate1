1234c064664d49251da5c9728502c2426116954b
Maxim Okorokov <okoro99@mail.ru>
2022-04-09 12:23:13 +0300

    сделал saveUser

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -26,6 +26,19 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("INSERT INTO users (name,lastName,age) " +
+                             "VALUES (?,?,?) ")) {
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,lastName);
+            preparedStatement.setByte(3,age);
+            preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
