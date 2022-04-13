05c69f05746d4599db90c0d17ba8ca317785349a
Maxim Okorokov <okoro99@mail.ru>
2022-04-13 11:42:58 +0300

    сделал utill

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,28 +1,68 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.tuple.Property;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
 
-   private static final String dbURL = "jdbc:mysql://localhost:3306/mydb1";
-   private static final String dbUserName = "root";
-   private static final String dbPassword = "Maxim_180299";
+    private static final String dbURL = "jdbc:mysql://localhost:3306/mydb1";
+    private static final String dbUserName = "root";
+    private static final String dbPassword = "Maxim_180299";
 
+    private static SessionFactory sessionFactory;
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         Connection connection = null;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(dbURL,dbUserName,dbPassword);
+            connection = DriverManager.getConnection(dbURL, dbUserName, dbPassword);
 
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Не подключился к БД");
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null){
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL,dbURL);
+                properties.put(Environment.USER,dbUserName);
+                properties.put(Environment.PASS,dbPassword);
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(registry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
