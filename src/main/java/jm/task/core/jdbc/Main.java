adfaeff642ca944a6aa18296c9c6690e85432321
Maxim Okorokov <okoro99@mail.ru>
2022-04-15 16:39:25 +0300

    в main исправл на интерфейс

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -13,31 +13,31 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
 
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
 
-        userDaoHibernate.createUsersTable();
+        userDao.createUsersTable();
 
         User user1 = new User("Name1", "LastName1", (byte) 20);
         User user2 = new User("Name2", "LastName2", (byte) 24);
         User user3 = new User("Name3", "LastName3", (byte) 10);
         User user4 = new User("Name4", "LastName4", (byte) 44);
 
-        userDaoHibernate.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userDao.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
         System.out.println("User с именем – " + user1.getName() + " добавлен в базу данных");
-        userDaoHibernate.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userDao.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
         System.out.println("User с именем – " + user2.getName() + " добавлен в базу данных");
-        userDaoHibernate.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userDao.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
         System.out.println("User с именем – " + user3.getName() + " добавлен в базу данных");
-        userDaoHibernate.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        userDao.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
         System.out.println("User с именем – " + user4.getName() + " добавлен в базу данных");
 
-        userDaoHibernate.removeUserById(2);
+        userDao.removeUserById(2);
 
 
-        List<User> allUsers = userDaoHibernate.getAllUsers();
+        List<User> allUsers = userDao.getAllUsers();
         allUsers.forEach(System.out::println);
 
-        userDaoHibernate.dropUsersTable();
+        userDao.dropUsersTable();
 
 
     }
